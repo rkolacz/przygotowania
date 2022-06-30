cfae8b41edc6ceed18dfdb67b57432f29a4be243
Rafal <rafal.kolacz@outlook.com>
2022-06-30 21:40:02 +0200

    Add html to download-elements.html, little changes in download_elements.js

--- js/download_elements.js
+++ js/download_elements.js
@@ -1,25 +1,25 @@
 // plik js do podstrony download-elements.html
 
 // getElementById
-console.log('getElementById');
+console.log(`getElementById - wywołanie zmiennej 'test'`);
 
 const test = document.getElementById('idtodownload'); // pobieranie elementu id z pliku html
 console.log(test);
 
 // getElementsByTagName - używane przy 'żywych kolekcjach' - dodawanych elementów w DOMie a nie w html
-console.log('getElementsByTagName');
+console.log(`getElementsByTagName - wywołanie zmiennej 'test2'`);
 
 const test2 = document.getElementsByTagName('li');
 console.log(test2);
 
 //getElementsByClassName - używane przy 'żywych kolekcjach' - dodawanych elementów w DOMie a nie w html
-console.log('getElementsByClassName');
+console.log(`getElementsByClassName - wywołanie zmiennej 'test3'`);
 
 const test3 = document.getElementsByClassName('code');
 console.log(test3);
 
 // document.querySelector
-console.log('querySelector');
+console.log(`querySelector - wywołanie zmiennej 'ulList'`);
 
 const ulList = document.querySelector('ul.code--idclass'); // querySelector przeszukuje document i szuka ul z klasą code-idclass
 console.log(ulList);
@@ -29,6 +29,6 @@ console.log(liFirstElement);
 
 // w () w querySelector można pisać metody z css takie jak nth-child
 
-console.log('querySelectorAll');
+console.log(`querySelectorAll - wywołanie zmiennej 'liAllElements'`);
 const liAllElements = document.querySelectorAll('li'); // All szuka w document wszystkich li
 console.log(liAllElements);
